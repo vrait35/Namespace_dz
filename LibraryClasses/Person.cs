d699393de4685b81b28675af6c3bbd5dc2166f4c
vrait35 <vrait35@mail.ru>
2018-09-16 23:59:47 +0600

    dz_namespace

---
+++ LibraryClasses/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryClasses
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class Information
+    {
+        public static string FullName(Person person)
+        {
+            return person.FirstName +" "+ person.Name + " " + person.LastName;
+        }
+    }
+    public class ConstantPi
+    {
+        private const double Pi = 3.14;
+        public double PI { get { return Pi; } }
+        private const string Name = "Исаак Ньютон";
+        public string NAME { get { return Name; } }
+    }
+}
---
+++ Namespace/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryClasses;
+
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryClasses.Person person = new LibraryClasses.Person()
+            { FirstName = "Темирбулат", Name = "Ануар", LastName = "Жанабекович" };
+            Console.WriteLine("1.   Результат: "+LibraryClasses.Information.FullName(person));
+            ConstantPi pi = new ConstantPi();
+            Console.WriteLine("2.   "+pi.PI+" "+pi.NAME);
+        }
+    }
+}
